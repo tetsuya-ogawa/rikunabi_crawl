eefd4ccfe6ae3bae7a2ffd24b1233e6deed6fafe
PT1972 <tetsuya.ogawa87@gmail.com>
2017-09-07 09:28:13 +0900

    カラムの決定

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require 'anemone'
 require "google_drive"
 count = 1
-(1..290).each do |time|
+(1..2).each do |time|
   datas = []
   session = GoogleDrive::Session.from_service_account_key('keyfile.json')
-  ws = session.spreadsheet_by_key('1Dplo6n7XXrFtT1y_ieeeOizUN3mVvPHox2fRp74C-Pw').worksheets[0]
+  ws = session.spreadsheet_by_key('1Oao7PU6MW0oeDrmN_H5hUwASEMH-kdFyFsXwNXiUbO8').worksheets[0]
+  # ws = session.spreadsheet_by_key('1Dplo6n7XXrFtT1y_ieeeOizUN3mVvPHox2fRp74C-Pw').worksheets[0]
   Anemone.crawl("https://job.rikunabi.com/2018/s/__________/?moduleCd=2&isc=ps055&pn=#{time}", delay: 1) do |anemone|
     # クロールするごとに呼び出される
     anemone.focus_crawl do |page|
@@ -19,9 +20,12 @@ count = 1
         '業種' => page.doc.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[3]/table/tr[1]/td/div[1]').text,
         '職種' => page.doc.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[3]/table/tr/td/div[2]').text,
         '本社' => page.doc.xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div[3]/table/tr[2]/td/div[1]').text,
-        '従業員数' => page.doc.xpath('/html/body/div[1]/div[2]/div').text.match(/\n.*従業員数.*\n.+\n/).to_s.gsub(/\r/, ',').gsub(/(従業員数|社員数|\n)/, ''),
+        '従業員数' => page.doc.xpath('/html/body/div[1]/div[2]/div').text.match(/(\n.*従業員数.*\n.+\n|\n.*社員数.*\n.+\n)/).to_s.tr("０-９", "0-9").gsub(/\r/, ',').gsub(/(従業員数|社員数|\n)/, '').match(/[\d,]+/),
         '募集人数' => page.doc.xpath('/html/body/div[1]/div[2]/div[1]/div[2]/table/tr/td/div').text,
-        'URL' => page.url.to_s
+        '更新日' => 'test',
+        'URL' => page.url.to_s,
+        '従業員数（原文）' => page.doc.xpath('/html/body/div[1]/div[2]/div').text.match(/(\n.*従業員数.*\n.+\n|\n.*社員数.*\n.+\n)/).to_s.gsub(/\r/, ',').gsub(/(従業員数|社員数|\n)/, ''),
+        '連絡先' => page.doc.xpath('//*[@id="company-data04"]/div')&.text.tr("０-９", "0-9").to_s
       }
       datas << data
       puts data
@@ -43,17 +47,8 @@ count = 1
           [k.to_s => v.to_s]
         end
       )
+      ws.save
     end
-    # session = GoogleDrive::Session.from_service_account_key('syusyokushien.json')
-    # ws = session.spreadsheet_by_key('1Dplo6n7XXrFtT1y_ieeeOizUN3mVvPHox2fRp74C-Pw').worksheets[0]
-    # datas.each do |data|
-    #   ws.list.push(
-    #     data.each do |k, v|
-    #       [k.to_s => v.to_s]
-    #     end
-    #   )
-    # end
-    # ws.save
   end
-  ws.save
+  # ws.save
 end
